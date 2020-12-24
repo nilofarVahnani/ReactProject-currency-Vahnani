07fe202834f1ee2f87307b1076671e852e96da62
nilofarvahnani <nilofarvahnani@gmail.com>
2020-12-24 23:02:35 +0330

    third commit

--- App.js
+++ App.js
@@ -6,8 +6,41 @@ function App() {
   return (
     <div className="app">
       <main>
+      <div className="main-text">قیمت خرید و فروش ارز</div>
         <div className="main-page">
-
+        <div className="dollar-kh">دلار امریکا خرید</div>
+        <div className="dollar-f">دلار امریکا فروش</div>
+        <div className="euro-kh">یورو خرید</div>
+        <div className="euro-f">یورو فروش</div>
+        <div className="derham-kh">درهم خرید</div>
+        <div className="derham-f">درهم فروش</div>
+        <div className="ropie-kh">روپیه خرید</div>
+        <div className="ropie-f"></div>
+        <div className="lir-kh"></div>
+        <div className="lir-f"></div>
+        <div className="robl-kh"></div>
+        <div className="robl-f"></div>
+        <div className="yoan-kh"></div>
+        <div className="yoan-f"></div>
+        <div className="voon-kh"></div>
+        <div className="voon-f"></div>
+        <div className="frunc-kh"></div>
+        <div className="frunc-f"></div>
+        <div className="yen-kh"></div>
+        <div className="yen-f"></div>
+        <div className="dollar-canada-kh"></div>
+        <div className="dollar-canada-f"></div>
+        <div className="pond-kh"></div>
+        <div className="pond-f"></div>
+        <div className="koron-soed-kh"></div>
+        <div className="koron-soed-f"></div>
+        <div className="koron-norvezh-kh"></div>
+        <div className="koron-norvezh-f"></div>
+        <div className="dinar-kh"></div>
+        <div className="dinar-f"></div>
+        <div className="dollar-ostoralia-kh"></div>
+        <div className="dollar-ostoralia-f"></div>
+      
         </div>
       </main>
       
